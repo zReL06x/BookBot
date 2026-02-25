5eafe5bdc18fef5b8ccb0f6f118f537697809679
BL-User <179770272+zReL06x@users.noreply.github.com>
2026-02-25 23:07:00 +0000

    updated requirements.txt

---
+++ worker/scraper.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Accept-Language': 'en-US, en;q=0.5'
+    }
+
+    # Making the HTTP Request
+    webpage = requests.get(URL, headers=HEADERS)
+
+    # Creating the Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "lxml")
