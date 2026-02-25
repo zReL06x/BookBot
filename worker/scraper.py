f8fbd69efcbfc82d117856b6c212997dff41f5ef
BL-User <179770272+zReL06x@users.noreply.github.com>
2026-02-25 23:15:00 +0000

    updated scraper code

--- worker/scraper.py
+++ worker/scraper.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-HEADERS = {
+def scrapeUrl(url):
+    HEADERS = {
         'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
         'Accept-Language': 'en-US, en;q=0.5'
     }
-
+    
     # Making the HTTP Request
-    webpage = requests.get(URL, headers=HEADERS)
+    webpage = requests.get(url, headers=HEADERS)
 
     # Creating the Soup Object containing all data
     soup = BeautifulSoup(webpage.content, "lxml")
+    
+    title = soup.find("a", "novel-title")
+    content = soup.find_all("p")
+    
+    
+
+print(scrapeUrl("https://novelbin.com/b/my-living-shadow-system-devours-to-make-me-stronger/chapter-1-the-academys-weakest"))
